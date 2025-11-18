68add521cc48a7a0eb5632faeac2420c6dc98d72
delgadilloabel1-cmd <delgadilloabel1@gmail.com>
2025-11-18 10:09:06 -0500

    completed

--- index.js
+++ index.js
@@ -5,6 +5,41 @@
  */
 
 class Song {
+  #playCount = 0
+  #rating = 0
+  static playlist = []
+  constructor(title, artist) {
+    this.title = title
+    this.artist = artist
+    Song.playlist.push(this)
+  }
+  get playCount() {
+    return this.#playCount
+  }
+  get rating() {
+    return this.#rating
+  }
+  play() {
+    this.#playCount += 1
+    console.log(`Now playing: ${this.title} by ${this.artist}`)
+  }
+  rate(stars) {
+    this.#rating = stars
+    console.log(`You rated ${this.title} ${stars} stars`)
+  }
+  isPopular() {
+    if (this.#playCount >= 10) {
+      return true
+    } else {
+      return false
+    }
+  }
+  static getTotalSongs() {
+    return this.playlist.length
+  }
+  static findByTitle(title) {
+    return this.playlist.find(song => song.title === title)
+  }
   //write your code here
 }
 
